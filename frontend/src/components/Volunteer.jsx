1c6591667ce71d12283fe4f1106da6883a16456b
krishnamanc <126003154@sastra.ac.in>
2024-07-27 20:13:35 +0530

    new

--- frontend/src/components/Volunteer.jsx
+++ frontend/src/components/Volunteer.jsx
@@ -1,6 +1,15 @@
 // Desc: Volunteer component
 export const Volunteer = () => {
   return (
-    <div>Volunteer</div>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+    <body>
+      <h1>Volunteer</h1>
+    </body>
+    </html>
   )
 }
