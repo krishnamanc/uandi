da4edd556c2012bb6c2b42f82f03f0f095b32096
krishnamanc <126003154@sastra.ac.in>
2024-07-27 21:19:01 +0530

    new

--- frontend/src/components/Volunteer.jsx
+++ frontend/src/components/Volunteer.jsx
@@ -1,6 +1,15 @@
-// Desc: Volunteer component
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+
 export const Volunteer = () => {
   return (
-    <div>volunteer</div>
+    <div>
+      <h1>
+        Example heading
+        <Badge bg="secondary" as={Button}>
+          New
+        </Badge>
+      </h1>
+    </div>
   )
 }
